8a63181ed7322307a11c089ed7ee4789604b5fc2
kon9397 <kon9397@gmail.com>
2020-03-31 23:55:00 +0300

    did homework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 /* global module */
-const getObjectEntries = () => { /* your code here... */ };
-const getObjectKeys = () => { /* your code here... */ };
-const getObjectValues = () => { /* your code here... */ };
+const getObjectEntries = (obj) => {
+  const entriesArray = [];
+
+  for(const item in obj) {
+    entriesArray.push([item, obj[item]]);
+  }
+
+  return entriesArray;
+};
+const getObjectKeys = (obj) => {
+  const keysArray = [];
+
+  for(const item in obj) {
+    keysArray.push(item);
+  }
+
+  return keysArray;
+};
+const getObjectValues = (obj) => {
+  const valuesArray = [];
+
+  for(const item in obj) {
+    valuesArray.push(obj[item]);
+  }
+
+  return valuesArray;
+};
 
 module.exports = {
   getObjectEntries,
